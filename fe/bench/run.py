862d3853e0fb70a5ea8865b91768e7e9179b3d31
Antares-Wu <wulinlong1231@163.com>
2021-01-07 19:52:30 +0800

    modify bench test

--- fe/bench/run.py
+++ fe/bench/run.py
@@ -1,9 +1,20 @@
+
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+rootPath2 = os.path.split(rootPath)[0]
+sys.path.append(rootPath2)
+
 from fe.bench.workload import Workload
 from fe.bench.session import Session
+
 
 def run_bench():
     wl = Workload()
     wl.gen_database()
+    ss = Session(wl)
+    ss.gen_procedure()
 
     sessions = []
     for i in range(0, wl.session):
@@ -11,12 +22,11 @@ def run_bench():
         sessions.append(ss)
 
     for ss in sessions:
-        ss.start()
+        ss.run()
 
     for ss in sessions:
         ss.join()
 
 
 if __name__ == "__main__":
-    #serve.be_run()
     run_bench()
